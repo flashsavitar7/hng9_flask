525b99c58cf4ddd583a19e41e987ac56ec141cf8
flashsavitar7 <99418944+flashsavitar7@users.noreply.github.com>
2022-10-27 14:35:29 +0100

    nu

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app.config['JSON_SORT_KEYS'] = False
 @app.route('/', methods=['GET'])
 
 def hello():
-    data = {"SlackUsername":"Triumph Edet", "Age":17, "Backend":True, "Bio":"An aspiring backend engineer ready to face the world of tech"}
+    data = {"SlackUsername":"Triumph Edet", "Backend":True, "Age":17, "Bio":"An aspiring backend engineer ready to face the world of tech"}
     return data
 
 if __name__ == '__main__':
